326e03fd7ca8cf5dd0a5cee9e60f5e580c809a4a
ra.01 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-12-11 22:53:14 +0530

    Setup index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+// Description: This is the main file of the application
+
+// Mandatory imports
+const express = require("express");
+const cors = require("cors");
+// initialize environment variables present in .env file in the root directory
+require("dotenv").config();
+const bodyParser = require("body-parser");
+
+// Express application setup
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    parameterLimit: 100000000,
+    extended: true,
+  })
+);
+
+// CORS setup
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// configure router
+const router = express.Router();
+app.use("/yoga", router);
+
+//Connect To DataBase
+require("./db/db");
+
+router.get("/", async (req, res) => {
+  res.send({
+    message: "all good here",
+    application: "yoga-app",
+  });
+});
+
+// Routes for all purposes
+// require("./routes/user.routes")(router);
+
+app.listen(process.env.PORT, () => {
+  console.log("Listening to port ", process.env.PORT);
+});
