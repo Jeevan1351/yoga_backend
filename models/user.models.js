d7d3b6a0867a0115e97fe59d66f6a0c9d56116d3
ra.01 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-12-11 22:53:39 +0530

    Create user.models.js

---
+++ models/user.models.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+// User Schema
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    name: { type: String, required: true },
+    role: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
