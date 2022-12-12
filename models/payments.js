1efa98762f67bd4b0bbdd23c499372d6df23ee3a
ra.01 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-12-12 23:14:47 +0530

    Payments model created

---
+++ models/payments.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+// Payment Schema
+const PaymentSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true },
+    year: { type: Number, required: true },
+    month: { type: Number, required: true },
+    status: { type: String, required: true },
+    amount: { type: Number, required: true },
+  },
+  { timestamps: true }
+);
+
+PaymentSchema.index({ email: 1, year: 1, month: 1, success }, { unique: true });
+const Payment = mongoose.model("Payment", PaymentSchema);
+
+module.exports = Payment;
