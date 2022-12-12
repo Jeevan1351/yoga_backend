2e3ac05bcc5a327f0648a69d767897868e446262
ra.01 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-12-12 21:53:46 +0530

    made parameterized middleware - authenticateToken

--- helpers/authenticateToken.js
+++ helpers/authenticateToken.js
@@ -2,24 +2,29 @@
 const jwt = require("jsonwebtoken");
 
 // Middleware to authenticate token
-const authenticateToken = (req, res, next) => {
-  // Get auth header value
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  // console.log("token", token);
+  const authenticateToken = (param) => {
+  return (req, res, next) => {
+    // Get auth header value
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    // console.log("token", token);
 
-  // Check if token is not there
-  if (token == null) return res.sendStatus(401);
+    // Check if token is not there
+    if (token == null) return res.sendStatus(401);
 
-  // Verify token
-  jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, user) => {
-    if (err) {
-      console.log(err.message);
-      return res.sendStatus(403);
-    }
-    req.user = user;
-    next();
-  });
-};
+    // Verify token
+    jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, user) => {
+      if (err) {
+        console.log(err.message);
+        return res.sendStatus(403);
+      }
+      if (param === "admin" && user.role !== "admin") {
+        return res.sendStatus(403);
+      }
+      req.user = user;
+      next();
+    });
+  };
+}
 
 module.exports = authenticateToken
