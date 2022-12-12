59daa15285c5bce1fed9618052ba1477505e9b27
ra.01 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-12-12 22:18:31 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,15 +1,18 @@
 const userController = require('../controllers/user.controller');
-const middleLogger = require('../helpers/middleLogger');
+// const middleLogger = require('../helpers/middleLogger');
 const authenticateToken = require('../helpers/authenticateToken');
 
 module.exports = (app) => {
 
     // Login a user
-    app.post('/users/login', middleLogger, userController.loginUser);
+    app.post('/users/login', userController.loginUser);
+
+    // Create a new user
+    app.post("/users/create", userController.createUser);
     
     // Update password
-    app.post('/users/update-password', middleLogger, userController.updatePassword);
+    app.post('/users/update-password', userController.updatePassword);
     
     //Create admin user
-    app.post('/users/create-admin', authenticateToken("admin"), middleLogger, userController.createAdminUser);
+    app.post('/users/create-admin', authenticateToken("admin"), userController.createAdminUser);
 }
