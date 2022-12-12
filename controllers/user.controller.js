7576e66f07a1071da3374efb1f09611828e8af24
ra.01 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-12-12 21:54:14 +0530

    added user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,67 @@
+const UserServices = require("../services/user.services");
+
+// Verify credentials of existing user and provide token
+exports.loginUser = async (req, res) => {
+  try {
+    var user = await UserServices.getUser(req.body);
+    if (user !== null) {
+      return res.status(201).json({
+        status: 201,
+        data: user,
+        message: "User logged in successfully",
+      });
+    } else {
+      return res.status(403).json({
+        status: 403,
+        message: "Invalid Credentials",
+      });
+    }
+  } catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+};
+
+// Update the password of existing user
+exports.updatePassword = async (req, res) => {
+  try {
+    const data = await UserServices.updatePassword(req.body);
+    if (data !== null) {
+      return res.status(200).json({
+        status: 200,
+        data: data,
+        message: "Password updated successfully",
+      });
+    } else {
+      return res.status(400).json({
+        status: 400,
+        message: "Password not updated",
+      });
+    }
+  }
+  catch (e) {
+    console.log(e)
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+}
+
+// Create new admin user
+exports.createAdminUser = async (req, res) => {
+  try {
+    const data = await UserServices.createAdminUser(req.body);
+    if (data !== null) {
+      return res.status(200).json({
+        status: 200,
+        data: data,
+        message: "Admin user created successfully",
+      });
+    } else {
+      return res.status(400).json({
+        status: 400,
+        message: "Admin user not created",
+      });
+    }
+  }
+  catch (e) {
+    return res.status(400).json({ status: 400, message: e.message });
+  }
+}
