954f1f204d1497c270f5f9996125918baaded29d
ra.01 <vijayeshjeevan.cs19@bmsce.ac.in>
2022-12-12 17:35:32 +0530

    Helper to authenticate token

---
+++ helpers/authenticateToken.js
@@ -0,0 +1,25 @@
+// const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+
+// Middleware to authenticate token
+const authenticateToken = (req, res, next) => {
+  // Get auth header value
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  // console.log("token", token);
+
+  // Check if token is not there
+  if (token == null) return res.sendStatus(401);
+
+  // Verify token
+  jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, user) => {
+    if (err) {
+      console.log(err.message);
+      return res.sendStatus(403);
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken
